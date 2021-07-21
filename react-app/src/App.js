ab082d0d9213e9d9e995b02611b6df62329143ba
skawlgns <skawlgns@kakao.com>
2021-07-21 11:33:58 +0900

    CRUD

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,55 +1,104 @@
 import React, { Component } from "react";
-import "./App.css";
 import TOC from "./components/TOC";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+import UpdateContent from "./components/UpdateContent";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
+import "./App.css";
 
 class App extends Component {
-  constructor(probs) {
-    super(probs);
+  constructor(props) {
+    super(props);
+    this.max_content_id = 3;
     this.state = {
-      mode: "read",
-      selected_content_id: 2,
+      mode: "welcome",
+      selected_content_id: 0,
       subject: { title: "WEB", sub: "World Wide Web!" },
-      welcome: { title: "WELCOME", desc: "Hello, React!" },
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
       contents: [
-        { id: 1, title: "HTML", desc: "HTML is HyperText..." },
-        { id: 2, title: "CSS", desc: "CSS is for design..." },
-        {
-          id: 3,
-          title: "JavaScript",
-          desc: "JavaScript is for interactive...",
-        },
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
       ],
     };
   }
-  render() {
-    let _title,
-      _desc = null;
+  getReadContent() {
+    var i = 0;
+    while (i < this.state.contents.length) {
+      var data = this.state.contents[i];
+      if (data.id === this.state.selected_content_id) {
+        return data;
+        break;
+      }
+      i = i + 1;
+    }
+  }
+  getContent() {
+    var _title,
+      _desc,
+      _article = null;
     if (this.state.mode === "welcome") {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>;
     } else if (this.state.mode === "read") {
-      let i = 0;
-      while (i < this.state.contents.length) {
-        let data = this.state.contents[i];
-        if (data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i++;
-      }
+      var _content = this.getReadContent();
+      _article = (
+        <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
+      );
+    } else if (this.state.mode === "create") {
+      _article = (
+        <CreateContent
+          onSubmit={function (_title, _desc) {
+            this.max_content_id = this.max_content_id + 1;
+            var _contents = Array.from(this.state.contents);
+            _contents.push({
+              id: this.max_content_id,
+              title: _title,
+              desc: _desc,
+            });
+            this.setState({
+              contents: _contents,
+              mode: "read",
+              selected_content_id: this.max_content_id,
+            });
+          }.bind(this)}
+        ></CreateContent>
+      );
+    } else if (this.state.mode === "update") {
+      _content = this.getReadContent();
+      _article = (
+        <UpdateContent
+          data={_content}
+          onSubmit={function (_id, _title, _desc) {
+            var _contents = Array.from(this.state.contents);
+            var i = 0;
+            while (i < _contents.length) {
+              if (_contents[i].id === _id) {
+                _contents[i] = { id: _id, title: _title, desc: _desc };
+                break;
+              }
+              i = i + 1;
+            }
+            this.setState({
+              contents: _contents,
+              mode: "read",
+            });
+          }.bind(this)}
+        ></UpdateContent>
+      );
     }
+    return _article;
+  }
+  render() {
     return (
-      <div className="app">
+      <div className="App">
         <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
           onChangePage={function () {
-            this.setState({
-              mode: "welcome",
-            });
+            this.setState({ mode: "welcome" });
           }.bind(this)}
         ></Subject>
         <TOC
@@ -61,7 +110,33 @@ class App extends Component {
           }.bind(this)}
           data={this.state.contents}
         ></TOC>
-        <Content title={_title} desc={_desc}></Content>
+        <Control
+          onChangeMode={function (_mode) {
+            if (_mode === "delete") {
+              if (window.confirm("정말입니까?")) {
+                var _contents = Array.from(this.state.contents);
+                var i = 0;
+                while (i < _contents.length) {
+                  if (_contents[i].id === this.state.selected_content_id) {
+                    _contents.splice(i, 1);
+                    break;
+                  }
+                  i = i + 1;
+                }
+                this.setState({
+                  mode: "welcome",
+                  contents: _contents,
+                });
+                alert("삭제되었습니다!");
+              }
+            } else {
+              this.setState({
+                mode: _mode,
+              });
+            }
+          }.bind(this)}
+        ></Control>
+        {this.getContent()}
       </div>
     );
   }
