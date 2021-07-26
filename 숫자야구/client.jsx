bdffffe59e455f552a1a5d3e4488ccf98525b4c2
skawlgns <skawlgns@kakao.com>
2021-07-26 10:47:59 +0900

    숫자야구

--- 숫자야구/client.jsx
+++ 숫자야구/client.jsx
@@ -1,6 +1,6 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
 
-const NumberBaseball = require("./NumberBaseball");
+import NumberBaseball from "./NumberBaseball";
 
 ReactDOM.render(<NumberBaseball />, document.querySelector("#root"));
