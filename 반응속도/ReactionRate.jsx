4db2934da29896dd7db88f64b12bb72eaf32a0e8
skawlgns <skawlgns@kakao.com>
2021-07-28 09:29:41 +0900

    Hooks.ver

--- 반응속도/ReactionRate.jsx
+++ 반응속도/ReactionRate.jsx
@@ -1,75 +1,57 @@
-import React, { Component } from "react";
+import React, { useState, useRef } from "react";
 
-class ReactionRate extends Component {
-  state = {
-    state: "waiting",
-    message: "클릭하면 시작합니다",
-    result: [],
-  };
-  timeout;
-  startTime;
-  endTime;
-  onClickScreen = () => {
-    const { state } = this.state;
+const ReactionRate = () => {
+  const [state, setState] = useState("waiting");
+  const [message, setMessage] = useState("클릭하면 시작합니다");
+  const [result, setResult] = useState([]);
+  const timeout = useRef(null);
+  const startTime = useRef(0);
+  const endTime = useRef(0);
+
+  const onClickScreen = () => {
     if (state == "waiting") {
-      this.setState({
-        state: "ready",
-        message: "초록색이 되면 클릭",
-      });
-      this.timeout = setTimeout(() => {
-        this.setState({
-          state: "now",
-          message: "클릭하세요!",
-        });
-
-        this.startTime = new Date();
+      timeout.current = setTimeout(() => {
+        setState("now");
+        setMessage("클릭하세요!");
+        startTime.current = new Date();
       }, Math.floor(Math.random() * 1000) + 2000); //2~3초 사이
+      setState("ready");
+      setMessage("초록색이 되면 클릭");
     } else if (state === "ready") {
-      clearTimeout(this.timeout);
+      clearTimeout(timeout.current);
       alert("너무 급했어요!");
-      this.setState({
-        state: "waiting",
-        message: "다시 클릭하세요!",
-      });
+      setState("waiting");
+      setMessage("다시 클릭하세요!");
     } else if (state === "now") {
-      this.endTime = new Date();
-      this.setState((prevState) => {
-        return {
-          state: "waiting",
-          message: "클릭하면 시작합니다",
-          result: [...prevState.result, this.endTime - this.startTime],
-        };
+      endTime.current = new Date();
+      setState("waiting");
+      setMessage("클릭하면 시작합니다");
+      setResult((prevResult) => {
+        return [...prevResult, endTime.current - startTime.current];
       });
     }
   };
 
-  onReset = () => {
-    this.setState({
-      result: [],
-    });
+  const onReset = () => {
+    setResult([]);
   };
 
-  renderAverage = () => {
-    const { result } = this.state;
+  const renderAverage = () => {
     return result.length === 0 ? null : (
       <>
         <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
-        <button onClick={this.onReset}>리셋</button>
+        <button onClick={onReset}>리셋</button>
       </>
     );
   };
 
-  render() {
-    const { state, message } = this.state;
-    return (
-      <>
-        <div id="screen" className={state} onClick={this.onClickScreen}>
-          {message}
-        </div>
-        {this.renderAverage()}
-      </>
-    );
-  }
-}
-
+  return (
+    <>
+      <div id="screen" className={state} onClick={onClickScreen}>
+        {message}
+      </div>
+      {renderAverage()}
+    </>
+  );
+};
 export default ReactionRate;
