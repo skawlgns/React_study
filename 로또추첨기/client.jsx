94640470dca9336e28e758350edbddb1538cdaf1
skawlgns <skawlgns@kakao.com>
2021-08-04 11:56:27 +0900

    '로또추첨기.1

---
+++ 로또추첨기/client.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import { hot } from "react-hot-loader/root";
+import Lotto from "./Lotto.jsx";
+
+// const Hot = hot(Lotto);
+
+ReactDOM.render(<Lotto />, document.querySelector("#root"));
